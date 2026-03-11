2c4e849027049949d745cc121e467448fd347365
Olayiwole Samuel <arnoldsammy76@gmail.com>
2026-03-12 00:54:28 +0100

    commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
+
+import Home from "./pages/Home";
+import AboutPage from "./pages/AboutPage.jsx";
+import ServicesPage from "./pages/ServicesPage.jsx";
+const App = () => {
+    return (
+        <div className="min-h-screen bg-black text-white">
+
+            <Navbar />
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<AboutPage/>}/>
+                <Route path="/services" element={<ServicesPage/>}/>
+            </Routes>
+
+        </div>
+    );
+};
+
+export default App;
